eacb8781e930b43bfcb48a4856e26c26c98a0027
MatVira <mviracucha14@gmail.com>
2024-03-19 23:17:35 -0500

    19/03/2024

---
+++ bucles.py
@@ -0,0 +1,22 @@
+'''
+
+Escribir un programa que lea un entero positivo,N 
+, introducido por el usuario y después muestre en pantalla 
+la suma de todos los enteros desde 1 hasta 
+N. La suma de los primeros enteros positivos puede ser 
+calculada de la siguiente forma:
+
+sumatoria= n(n+1)/2
+
+'''
+
+
+
+numero= int(input("ingrese el numero entero: "))
+
+sumatoria= (numero*(numero+1))/(2)
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,128 @@
+#comentarios con numeral
+
+'''
+comentarios de mutiples lineas
+
+'''
+
+
+# numericas, string, clear, booleanas.
+
+
+#para definir el python variables de consta de la siguiente formuula
+
+#int numero1= 5;
+# <nombre_de_la_variable> = <variable de icha constante>;
+
+
+
+#numero1 =10 ;
+
+#numero2= 15 ;
+
+#numero3= numero1+numero2 ;
+
+#print(numero3) ;
+
+#print("hola mundo");
+
+
+
+
+# crear una calculadora suma y resta
+# sumar, restar, multiplicar y dividir dos números e imprimir el resultado
+
+#1. pedir datos al usuario
+#2. imprimir el resultado de la suma, resta, multiplicacion y division; segun este ejemplo:
+# suma: resultyado de la suma; y lo mismo para los siguientes.
+
+
+# "pedir datos al usuario" --> inputn() "solo recibe datos string (str)"
+
+# conversion de tipo de datos    int(input())-> "5" -> 5
+
+
+
+
+#print("ingrese el num1: ")
+#numero1= int(input())
+
+#print("ingrese el numero2: ")
+#numero2= int(input())
+
+#print("suma=", numero1+numero2)
+#print("producto= ", numero1*numero2)
+#print("resta= ", numero1-numero2)
+#print("division: ", numero1/numero2)
+
+
+
+
+#print("numero ya guardado", numero1)
+#print(type(numero1))  #--> imprimir el tipo de dato de la variable
+
+
+
+# cadenas de caracteres (str->strings)
+
+
+#crear un programa que me imprima los dias de la semana con el siguiente saludo
+# "buenos dias a, hoy es B" -> sien "A nombre del usuario y "B el dia de la semana"
+
+'''
+#1. crea o plantear los datos o variables
+
+#2. pedir_nombre_usuario.
+
+#3. imprimir salida con nombre y los 7 dias de la semana
+
+#print("ingrese el nombre del usuario")
+#nombreUsuario= input()
+
+#print("hola ", nombreUsuario, "hoy es lunes" )
+#print("hola ", nombreUsuario, "hoy es martes" )
+#print("hola ", nombreUsuario, "hoy es miercoles" )
+#print("hola ", nombreUsuario, "hoy es jueves" )
+#print("hola ", nombreUsuario, "hoy es viuernes" )
+#print("hola ", nombreUsuario, "hoy es sabado" )
+#print("hola ", nombreUsuario, "hoy es domingo" )
+'''
+
+'''
+
+Escribir un programa que muestre por pantalla el resultado de la
+siguienteoperación aritmética 
+
+'''
+
+
+#1. variable resultado 
+
+#resultado=((3+2)/(2*5))**2
+#print(resultado)
+
+
+'''
+
+Escribir un programa que pregunte al usuario por el número de horas
+trabajadas y el coste por hora. Después debe mostrar por pantalla la
+paga que le corresponde.
+
+Print(coste*num_horas)
+
+'''
+
+#costeHora= int(input("ingrese el coste por numero de hora de trabajo: "))
+#numeroHoras= int(input("ingrese el numero de horas trabajadas: "))
+
+#print("tu total de paga de horas trabajas es: ", costeHora*numeroHoras)
+
+
+'''
+
+Escribir un programa que lea un entero positivo, 
+, introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta 
+. La suma de los 
+ primeros enteros positivos puede ser calculada de la siguiente forma:
+
+'''
